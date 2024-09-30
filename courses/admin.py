ccaec153f09236f78219f267536557c689956a92
A Nuri <ahmettonuri@gmail.com>
2024-09-30 20:00:22 +0300

    nav bar edited

--- courses/admin.py
+++ courses/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Course, Category, Slider
 from django_summernote.admin import SummernoteModelAdmin #summernote1
-
+#%pip install django-summernote #summernote0
 # Register your models here.
 
 
